1cf3ac3cc4fe1dc31b97e6042d027eed4612bdb0
Owen Rubel <orubel@gmail.com>
2022-09-01 18:34:27 -0700

    fixed class that was not completely rewritten

--- src/main/java/demo/application/properties/yaml/factory/YamlPropertySourceFactory.java
+++ src/main/java/demo/application/properties/yaml/factory/YamlPropertySourceFactory.java
@@ -8,22 +8,26 @@ import org.springframework.core.io.support.PropertySourceFactory;
 
 import java.io.IOException;
 import java.util.Properties;
+import java.util.*;
 
 public class YamlPropertySourceFactory implements PropertySourceFactory {
 
     @Override
     public PropertySource<?> createPropertySource(String name, EncodedResource encodedResource) throws IOException {
+        PropertySource<?> ps = new PropertiesPropertySource("", new java.util.Properties());
         try {
             YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
             factory.setResources(encodedResource.getResource());
 
             Properties properties = factory.getObject();
 
-            return new PropertiesPropertySource(encodedResource.getResource().getFilename(), properties);
+            ps = new PropertiesPropertySource(encodedResource.getResource().getFilename(), properties);
         }catch(Exception e){
-            String fileName = encodedResource.getResource().getFilename()
-            println("cannot load '${fileName}' : " + e)
+            String fileName = encodedResource.getResource().getFilename();
+            System.out.println("cannot load '${fileName}' : " + e);
         }
+
+        return ps;
     }
 
 }
