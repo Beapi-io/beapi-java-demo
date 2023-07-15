3f1089778c8c8861bf01adf085f3253ea68356a7
Owen Rubel <orubel@gmail.com>
2023-07-15 11:51:27 -0700

    adding in example classes and updating gradle

--- src/main/java/demo/application/repositories/CompanyRepository.java
+++ src/main/java/demo/application/repositories/CompanyRepository.java
@@ -1,9 +1,7 @@
 package demo.application.repositories;
 
-import demo.application.domain.Authority;
+
 import demo.application.domain.Company;
-import demo.application.domain.User;
-import demo.application.domain.UserAuthority;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.*;
